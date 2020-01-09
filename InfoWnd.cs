fd64e7916ffeec2c5a1e4ef23b22e73b72c5824b
Guriy Samarin <guriysamarin@outlook.com>
2020-01-09 12:02:12 +0300

    Add project files.

---
+++ InfoWnd.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using CheckDotNet.Properties;
+
+namespace CheckDotNet
+{
+    public partial class InfoWnd : Form
+    {
+        public InfoWnd()
+        {
+            InitializeComponent();
+
+            lblInfo.Text = Resources.InfoWnd_InfoWnd;
+
+            lblProduct.Text = Resources.InfoWnd_Product;
+            lblVersion.Text = Resources.InfoWnd_Version + Application.ProductVersion;
+            lblFirma.Text = Resources.InfoWnd_Firma;
+            lblCopyright.Text = Resources.InfoWnd_Copyright;
+
+            LinkLabel.Link link = new LinkLabel.Link();
+            link.LinkData = "http://leochapiro.de";
+            linkLabel.Links.Add(link);
+        }
+
+        private void btnClose_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(e.Link.LinkData.ToString());
+        }
+    }
+}
